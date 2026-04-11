727627f0acf1c102d905cef8345eac767f2d9ba8
smp4000 <40184290+smp4000@users.noreply.github.com>
2026-04-11 21:11:43 +0200

    Fix: HasUuids Trait in LabelTemplate (UUID auto-generierung)

--- app/Models/LabelTemplate.php
+++ app/Models/LabelTemplate.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -14,7 +15,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class LabelTemplate extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, HasUuids, SoftDeletes;
 
     protected $fillable = [
         'tenant_id',
